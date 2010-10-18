fd272280813fce3ee51957786f10a8568e4c92f4
Juan BC <jbc.develop@gmail.com>
2010-10-18 03:30:49 -0300

    moved into pyconar2010

--- pyconar2010/infopython/examples/twuser_ej.py
+++ pyconar2010/infopython/examples/twuser_ej.py
@@ -3,12 +3,12 @@ from pprint import pprint
 from infopython import session
 from infopython.isources import twitteruser
 
-session.set(compete_key = "5h88328y8wchp8gf8bgt4fx9",
-            twitter_key = "X8yUKlPyHmfMRQpV2gXWg",
-            twitter_secret = "71Q3lqNMlEEnSpa10ynmKTXiuQxB0Yg051vecVWzOPc",
-            twitter_user_key = "57517417-V1F43HjYyad6HlaQqFpXrtAre3dbJPM9F56rcZI03",
-            twitter_user_secret = "DOMLETmR9XSEbb45l8fc5OGDsInbusP0wJd18dVQk",
-            klout_api_key = "d6zu25pnvf7ppz99925sg5xj")
+session.set(compete_key = "",
+            twitter_key = "",
+            twitter_secret = "",
+            twitter_user_key = "",
+            twitter_user_secret = "",
+            klout_api_key = "")
             
 yo = twitteruser.TwitterUser("leliel12")
 
