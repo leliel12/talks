3a29890b7fc7e02dd652b12048894a8f6d750604
Juan BC <jbc.develop@gmail.com>
2011-04-13 14:58:51 -0300

    add ejemplo y fixed algunos errores

---
+++ Unicode2011/datos/ejemplos/p1.py
@@ -0,0 +1,24 @@
+import yaml
+
+
+class Direccion(object):
+    
+    def __init__(self, tipo, desc):
+        self.tipo = tipo
+        self.desc = desc
+                
+
+class Persona(object):
+    
+    def __init__(self, name, direcciones):
+        self.name = name
+        self.direcciones = direcciones
+
+dir1 = Direccion("personal", "fake 123")
+dir2 = Direccion("laboral", "real 456")
+
+persona = Persona("Tito Puente", [dir1, dir2])
+
+src = yaml.dump(persona)
+
+print src
