a9622474c38dded3fbd53f49e238341600dfa7fc
Juan BC <jbc.develop@gmail.com>
2012-10-14 19:17:51 -0300

    added example of table creation

--- pyday_rafaela2012/peewee/dom.py
+++ pyday_rafaela2012/peewee/dom.py
@@ -9,15 +9,21 @@ class ExampleModel(Model):
     class Meta:
         database = example_db
 
-
 class User(ExampleModel):
     name = CharField()
+
+    def __unicode__(self):
+        return "<User '{}'>".format(self.name)
 
 
 class Car(ExampleModel):
     model = CharField(null=True)
     plate = CharField(unique=True)
     user = ForeignKeyField(User, related_name="cars")
+
+    def __unicode__(self):
+        return "<Car '{}-{}'>".format(self.model, self.plate)
+
 
 # Creamos las tablas si no existen
 User.create_table(fail_silently=True)
