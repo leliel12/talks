e19afa0f27dfc742c749a3493afb948a19ef3c7c
Juan BC <jbc.develop@gmail.com>
2012-10-20 15:52:14 -0300

    added example

---
+++ pyday_rafaela2012/pycante/dialog.py
@@ -0,0 +1,36 @@
+
+import os
+
+from PyQt4 import QtCore, QtGui
+
+import pycante
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+
+UI_DIR = pycante.EDir(PATH)
+
+
+
+
+
+
+class Dialog(UI_DIR("Dialog.ui")):
+
+    def on_pushButton_clicked(self):
+        text = self.lineEdit.text()
+        self.label.setText("Hola " + unicode(text) + "!")
+
+
+
+
+
+
+
+
+
+
+
+app = QtGui.QApplication([])
+d = Dialog()
+d.show()
+app.exec_()
